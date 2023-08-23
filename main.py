7397b5a0d75ca11a607579b9b789fd73309c8a04
anzhltv <altuyevaab@gmail.com>
2023-08-23 11:05:22 +0300

    isort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 from datetime import datetime
+
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,12 +14,14 @@ def get_screen_resolution():
     screen_width, screen_height = pyautogui.size()
     return screen_width, screen_height
 
+
 def if_edge(y):
     if y < 50:
         y1 = y + 50
     else:
         y1 = y - 15
     return y1
+
 
 # яркость/контраст в доработке
 def objective(I_new, I_et, N):
@@ -76,36 +79,39 @@ def image_hist(frame_hist, arr_hist, count_id, frame, k, stop):
     # # time.sleep(3)
     # plt.close('all')
     # frame_hist = cv2.cvtColor(frame_hist, cv2.COLOR_BGR2RGB)
-    if stop==False:
+    if stop == False:
         b, g, r = cv2.split(frame_hist)
         b_eq = cv2.equalizeHist(b)
         g_eq = cv2.equalizeHist(g)
         r_eq = cv2.equalizeHist(r)
         frame_hist = cv2.merge((b_eq, g_eq, r_eq))
         frame_hist = cv2.calcHist(
-            [frame_hist], [0, 1, 2], None, [160, 160, 160], [0, 256, 0, 256, 0, 256] )
+            [frame_hist], [0, 1, 2], None, [160, 160, 160], [0, 256, 0, 256, 0, 256]
+        )
         cv2.normalize(frame_hist, None, 0, 1.0, cv2.NORM_MINMAX)
         EPSILON = 1e-6
-        compare = cv2.compareHist(arr_hist[count_id][frame].astype(np.float32), frame_hist.astype(np.float32),
-                              cv2.HISTCMP_CORREL)
-        if k[0]>0:
+        compare = cv2.compareHist(
+            arr_hist[count_id][frame].astype(np.float32),
+            frame_hist.astype(np.float32),
+            cv2.HISTCMP_CORREL,
+        )
+        if k[0] > 0:
             if np.all(np.abs(arr_hist[count_id]) < EPSILON):
                 arr_hist[count_id][frame] = frame_hist
             else:
                 arr_hist[count_id][frame] = (
-                        alpha * (frame_hist) + (1 - alpha) * arr_hist[count_id][frame]
+                    alpha * (frame_hist) + (1 - alpha) * arr_hist[count_id][frame]
                 )
-            k[0]-=1
+            k[0] -= 1
         else:
-            if (frame==0 and compare>0.1) or (frame==1 and compare>0.1):
+            if (frame == 0 and compare > 0.1) or (frame == 1 and compare > 0.1):
                 if np.all(np.abs(arr_hist[count_id]) < EPSILON):
                     arr_hist[count_id][frame] = frame_hist
                 else:
                     arr_hist[count_id][frame] = (
-                    alpha * (frame_hist) + (1 - alpha) * arr_hist[count_id][frame]
+                        alpha * (frame_hist) + (1 - alpha) * arr_hist[count_id][frame]
                     )
     return count_id
-
 
 
 path1 = "3.Camera 2017-05-29 16-23-04_137 [3m3s].avi"
@@ -119,14 +125,12 @@ I_et = cv2.VideoCapture("ex1_1.png")
 _, I_new = I_new.read()
 _, I_et = I_et.read()
 
-# object_detector1 = cv2.createBackgroundSubtractorMOG2(detectShadows=True, history=60000, varThreshold=10)
-# object_detector2 = cv2.createBackgroundSubtractorMOG2(detectShadows=True, history=40000, varThreshold=10)
 EPSILON = 1e-6
 width, height = get_screen_resolution()
 N = width * height
 size1 = int(round(0.02 * width * height))
 size2 = int(round(0.0034 * width * height))
-boxes_ids2_save=[]
+boxes_ids2_save = []
 id_save, id2_save, id1_save = (0, 0, 0)
 id2, id1 = (0, 0)
 id2_new, id1_new = (0, 0)
@@ -134,20 +138,20 @@ flag1, flag2 = (1, 1)
 correct_id2, correct_id1 = (0, 0)
 ret1, frame1_1 = cap1.read()
 ret2, frame2_2 = cap2.read()
-arr_id1=[0]*10
-arr_id2=[0]*10
+arr_id1 = [0] * 10
+arr_id2 = [0] * 10
 array_hist = np.zeros((200, 1, 160, 160, 160))
-k, k1, k2 = (0,0,0)
-global_id, global_id1, global_id2 = (0,0,0)
-opt_param1, opt_param2=(0.5, 0.21)
-count_same=0
-k=[30]
-k1=30
-id_number=0
-save_global_id1, save_global_id2=(0,0)
-stop1, stop2=(False, False)
-some=5
-output_frames=[]
+k, k1, k2 = (0, 0, 0)
+global_id, global_id1, global_id2 = (0, 0, 0)
+opt_param1, opt_param2 = (0.5, 0.21)
+count_same = 0
+k = [30]
+k1 = 30
+id_number = 0
+save_global_id1, save_global_id2 = (0, 0)
+stop1, stop2 = (False, False)
+some = 5
+output_frames = []
 while True:
     ret1, frame1 = cap1.read()
     ret2, frame2 = cap2.read()
@@ -208,10 +212,10 @@ while True:
                 if np.all(array_hist[correct_id1][0] < EPSILON):
                     array_hist[correct_id1][0] = array_hist[global_id1][0]
                 array_hist[global_id1][0] = 0
-                count_same+=1
+                count_same += 1
                 flag1 = 1
                 for i in range(len(arr_id1)):
-                    arr_id1[i]=0
+                    arr_id1[i] = 0
                 k[0] = 20
 
         # /////////////////////////////////
@@ -221,15 +225,15 @@ while True:
             array_hist[global_id2][0] = 0
             flag2 = 1
             if max(arr_id2) > 15:
-               count_same += 1
+                count_same += 1
             for i in range(len(arr_id2)):
                 arr_id2[i] = 0
             k[0] = 20
 
         global_id1 = id1 - count_same
-        if save_global_id1!=global_id1:
-            k[0]=20
-            if k1>0:
+        if save_global_id1 != global_id1:
+            k[0] = 20
+            if k1 > 0:
                 if max(arr_id1) > 15:
                     correct_id1 = arr_id1.index(max(arr_id1))
                 else:
@@ -237,7 +241,7 @@ while True:
                     flag1 = 1
         if flag1 != 2 and id1_save != id1:
             k1 = 30
-            stop1=False
+            stop1 = False
         id1_save = id1
         save_global_id1 = image_hist(frame_plt, array_hist, global_id1, 0, k, stop1)
         max1 = 0
@@ -249,7 +253,7 @@ while True:
                         array_hist[i][j].astype(np.float32),
                         cv2.HISTCMP_CORREL,
                     )
-                    if compareHist1 > opt_param1 and compareHist1!=1 :
+                    if compareHist1 > opt_param1 and compareHist1 != 1:
                         arr_id1[i] += 1
                         if max1 < compareHist1:
                             boxes_ids1[:][-1] = i
@@ -257,8 +261,8 @@ while True:
                             flag1 = 2
                             max1 = compareHist1
             k1 -= 1
-        if k1>0:
-            correct_id1=id1-count_same
+        if k1 > 0:
+            correct_id1 = id1 - count_same
         else:
             if max(arr_id1) >= 6:
                 correct_id1 = arr_id1.index(max(arr_id1))
@@ -273,7 +277,15 @@ while True:
                 flag1 = 3
             else:
                 id1 = id1 - count_same
-        cv2.putText(frame1, "Object " + str(id1), (x, y1), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2,)
+        cv2.putText(
+            frame1,
+            "Object " + str(id1),
+            (x, y1),
+            cv2.FONT_HERSHEY_PLAIN,
+            2,
+            (0, 0, 255),
+            2,
+        )
         cv2.rectangle(frame1, (x, y), (x + w, y + h), (255, 0, 0), 3)
         tracker2.id_count = tracker1.id_count
 
@@ -285,22 +297,22 @@ while True:
         x, y, w, h, id2 = box_id
         y1 = if_edge(y)
         frame_plt2 = roi2[y : y + h, x : x + w]
-        if flag1 == 2 and report==False:
+        if flag1 == 2 and report == False:
             if np.all(array_hist[correct_id1][0] < EPSILON):
                 array_hist[correct_id1][0] = array_hist[global_id - 1][0]
             array_hist[global_id - 1][0] = 0
             flag1 = 1
-            if report==False:
+            if report == False:
                 if max(arr_id1) > 15:
                     count_same += 1
                     for i in range(len(arr_id1)):
                         arr_id1[i] = 0
             k[0] = 20
         # ////////////////////////////////////////////
-        global_id2=id2-count_same
-        if save_global_id2!=global_id2:
-            k[0]=20
-            if k2>0:
+        global_id2 = id2 - count_same
+        if save_global_id2 != global_id2:
+            k[0] = 20
+            if k2 > 0:
                 if max(arr_id2) > 15:
                     correct_id2 = arr_id2.index(max(arr_id2))
                 else:
@@ -309,11 +321,11 @@ while True:
         max2 = 0
         if flag2 != 2:
             k2 = 30
-            stop2=False
+            stop2 = False
         save_global_id2 = image_hist(frame_plt2, array_hist, global_id2, 0, k, stop2)
         if k2 > 0:
-            arr_id=0
-            obj_k=0
+            arr_id = 0
+            obj_k = 0
             for i in range(global_id2):
                 for j in range(1):
                     compareHist2 = cv2.compareHist(
@@ -321,27 +333,24 @@ while True:
                         array_hist[i][j].astype(np.float32),
                         cv2.HISTCMP_CORREL,
                     )
-                    if compareHist2 > opt_param2 and compareHist2!=1:
-                        # arr_id2[i] += 1
+                    if compareHist2 > opt_param2 and compareHist2 != 1:
                         if max2 < compareHist2:
-                            # boxes_ids2[:][-1] = i
                             id2_new = id2
-                            # correct_id2 = i
                             flag2 = 2
                             max2 = compareHist2
-                            arr_id=i
-                            obj_k+=1
-            if obj_k>0:
+                            arr_id = i
+                            obj_k += 1
+            if obj_k > 0:
                 arr_id2[arr_id] += 1
             k2 -= 1
-        if k2>0:
-            correct_id2=id2-count_same
+        if k2 > 0:
+            correct_id2 = id2 - count_same
         else:
             if max(arr_id2) > 15:
-               correct_id2=arr_id2.index(max(arr_id2))
+                correct_id2 = arr_id2.index(max(arr_id2))
             else:
-                correct_id2=id2-count_same
-                flag2=1
+                correct_id2 = id2 - count_same
+                flag2 = 1
             stop2 = True
         if id2 == id2_new and flag2 == 2:
             id2 = correct_id2
@@ -359,14 +368,22 @@ while True:
             k[0] = 20
             for i in range(len(arr_id2)):
                 arr_id2[i] = 0
-        cv2.putText(roi2, "Object " + str(id2), (x, y1), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+        cv2.putText(
+            roi2,
+            "Object " + str(id2),
+            (x, y1),
+            cv2.FONT_HERSHEY_PLAIN,
+            2,
+            (0, 0, 255),
+            2,
+        )
         cv2.rectangle(roi2, (x, y), (x + w, y + h), (255, 0, 0), 3)
         tracker1.id_count = tracker2.id_count
-    resized_frame_1 = cv2.resize(frame1, (width // 2 , height // 2 ))
-    resized_frame_2 = cv2.resize(frame2, (width // 2 , height // 2))
+    resized_frame_1 = cv2.resize(frame1, (width // 2, height // 2))
+    resized_frame_2 = cv2.resize(frame2, (width // 2, height // 2))
     combined_frame = cv2.hconcat([resized_frame_1, resized_frame_2])
     output_frames.append(combined_frame)
-    cv2.imshow('combined', combined_frame)
+    cv2.imshow("combined", combined_frame)
     key = cv2.waitKey(30)
     if key == 27:
         break
